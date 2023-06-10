d5817e60afb20618dc17048e058f4ced35a35d51
Uttam <amiadlakha@gmail.com>
2023-06-10 18:02:15 +0530

    jun10/23/report

--- fashion_navya/fashion_navya/report/stock_transit__by_today/stock_transit__by_today.js
+++ fashion_navya/fashion_navya/report/stock_transit__by_today/stock_transit__by_today.js
@@ -1,9 +1,26 @@
 // Copyright (c) 2023, pawasthy11@gmail.com and contributors
 // For license information, please see license.txt
 /* eslint-disable */
-
 frappe.query_reports["Stock Transit  By Today"] = {
-	"filters": [
+    "filters": [
+			{
+				"label":"Stock Entry Type",
+				"fieldtype": "Link",
+				"fieldname": "stype",
+				"options":"Stock Entry Type"
+			},
+			{
+            "fieldname": "from_date",
+            "label": __("From Date"),
+            "fieldtype": "Date",
+            "default": frappe.datetime.get_today()
+        },
+        {
+            "fieldname": "to_date",
+            "label": __("To Date"),
+            "fieldtype": "Date",
+            "default": frappe.datetime.get_today()
+        },
 
-	]
+    ]
 };
--- fashion_navya/fashion_navya/report/stock_transit__by_today/stock_transit__by_today.py
+++ fashion_navya/fashion_navya/report/stock_transit__by_today/stock_transit__by_today.py
@@ -1,9 +1,93 @@
 # Copyright (c) 2023, pawasthy11@gmail.com and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+from frappe import _
+from frappe.utils import flt, time_diff_in_hours
+
 
 
 def execute(filters=None):
-	columns, data = [], []
+	columns = get_columns()
+	data = get_data(filters)
 	return columns, data
+
+
+
+def get_columns():
+	return [
+		{
+			"label": _("Stock Entry"),
+			"fieldtype": "Link",
+			"fieldname": "se",
+			"options": "Stock Entry",
+			"width":200,
+		},
+		{
+			"label": _("Stock Entry Type"),
+			"fieldtype": "Link",
+			"fieldname": "set",
+			"options":"Stock Entry Type",
+			"width":200,
+		},
+		{
+			"label": _("Created By"),
+			"fieldtype": "Data",
+			"fieldname": "cb",
+			"width":200,
+		},
+		{
+			"label": _("Creation Date"),
+			"fieldtype": "Date",
+			"fieldname": "cd",
+			"width":150,
+		},
+		{
+			"label": _("Item Count"),
+			"fieldtype": "Data",
+			"fieldname": "ic",
+			"width":90,
+		},
+
+
+
+
+		]
+
+
+
+def get_data(filters):
+	data = []
+	if not filters :
+		return []
+
+	if filters.from_date > filters.to_date:
+		frappe.msgprint(_("From Date can not be greater than To Date"))
+		return data
+
+	print(filters.from_date,'af')
+	print(filters.to_date,'at')
+
+	if filters.stype:
+		se=frappe.db.sql(""" select name, posting_date,stock_entry_type from `tabStock Entry` where docstatus=0 and posting_date between '{}' and '{}' and stock_entry_type='{}' """.format(filters.from_date,filters.to_date,filters.stype),as_dict=1)
+	else:
+		se=frappe.db.sql(""" select name, posting_date,stock_entry_type from `tabStock Entry` where docstatus=0 and posting_date between '{}' and '{}'  """.format(filters.from_date,filters.to_date),as_dict=1)
+
+	for i in se:
+		sedoc=frappe.get_doc("Stock Entry",i['name'])
+		items=sedoc.items
+		d={}
+		d['se']=i['name']
+		d['set']=sedoc.stock_entry_type
+		d['cb']=sedoc.owner
+		d['cd']=sedoc.posting_date
+		d['ic']=len(items)
+		data.append(d)
+
+
+
+
+
+
+
+	return data
