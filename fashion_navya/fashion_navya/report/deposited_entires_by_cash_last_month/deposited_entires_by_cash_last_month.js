54f3f1ed2d10fdb2323df4ba592c915f27fd7de3
Uttam <amiadlakha@gmail.com>
2023-07-10 15:41:52 +0530

    jul10

--- fashion_navya/fashion_navya/report/deposited_entires_by_cash_last_month/deposited_entires_by_cash_last_month.js
+++ fashion_navya/fashion_navya/report/deposited_entires_by_cash_last_month/deposited_entires_by_cash_last_month.js
@@ -4,6 +4,28 @@
 
 frappe.query_reports["Deposited Entires By cash last Month"] = {
 	"filters": [
+		{
+			fieldname: "mop",
+			label: __("Mode of Payment"),
+			fieldtype: "Link",
+			options: "Mode of Payment",
+			default:"Cash",
+			reqd: 1
+		},
+		{
+			fieldname:"from_date",
+			label: __("From Date"),
+			fieldtype: "Date",
+			default: frappe.datetime.add_months(frappe.datetime.month_start(), -1),
+			reqd: 1
+		},
+		{
+			fieldname:"to_date",
+			label: __("To Date"),
+			fieldtype: "Date",
+			default: frappe.datetime.add_days(frappe.datetime.month_start(), -1),
+			reqd: 1
+		},
 
 	]
 };
