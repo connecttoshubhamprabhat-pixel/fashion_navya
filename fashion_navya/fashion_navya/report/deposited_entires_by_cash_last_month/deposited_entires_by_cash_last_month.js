67f1ad4fe0702ed502193b14f29f9f5088c6550c
Navya <amiadlakha@gmail.com>
2023-07-10 14:29:55 +0530

    jul10

---
+++ fashion_navya/fashion_navya/report/deposited_entires_by_cash_last_month/deposited_entires_by_cash_last_month.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2023, pawasthy11@gmail.com and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Deposited Entires By cash last Month"] = {
+	"filters": [
+
+	]
+};
