a7d19fe126d0d9e50c695f5e8cdf37b559f94ceb
Uttam <amiadlakha@gmail.com>
2023-09-27 20:23:25 +0530

    sep27/23

--- fashion_navya/utils/doc_event/custom.py
+++ fashion_navya/utils/doc_event/custom.py
@@ -1,6 +1,8 @@
 import frappe
 from erpnext.stock.dashboard.item_dashboard import get_data
+from navya.api_folder.py.project import make_pattren_from_variant_so,bom_copy_so_enabled_item
 import json
+import re
 from navya.api_folder.py.item_variants import create_multiple_variants_custom,create_variant_custom
 
 
@@ -56,12 +58,11 @@ def make_rtw_item(doc,method):
 
 #sales ordr-sales order
 @frappe.whitelist(allow_guest=True)
-def make_rtw_item_pro(items=None):
-	if not frappe.db.exists("Item",items):
-		frappe.msgprint("Item Not exists")
+def make_rtw_item_so(items=None):
+	if not items:
+		return
 
-	items_list=[]
-	items_list.append(items)
+	items_list=json.loads(items)
 	for m in items_list:
 		item_doc=frappe.get_doc("Item",m)
 		if item_doc.item_group!="Sample" or  not item_doc.variant_of:
@@ -71,67 +72,92 @@ def make_rtw_item_pro(items=None):
 		if item_doc.rtw==1 or not  item_doc.variant_of:
 			return
 
+		template=frappe.get_doc("Item",item_doc.variant_of)
+		yes_item_group=[]
+		for u in template.attributes:
+			if u.attribute=="Item Group":
+				yes_item_group.append("yes")
+		if not yes_item_group:
+			row=template.append("attributes", {})
+			row.attribute="Item Group"
+		template.save(ignore_permissions=True)
 
 		check_smpl=item_doc.name.split("-")
 		if "RTW" in check_smpl:
 			tr3="-".join(check_smpl)
 			return tr3
-	
+
 		if "SMPL" in check_smpl:
 			index =check_smpl.index("SMPL")
 			check_smpl[index]="RTW"
 			jrtw="-".join(check_smpl)
 			if frappe.db.exists("Item",jrtw):
 				return jrtw
-			d={}
-			for m in item_doc.attributes:
-				if m.attribute!="Item Group":
-					d[m.attribute]=m.attribute_value
-			d['Item Group']="Ready To Wear"
-			variants=create_variant_custom(item_doc.variant_of,d)
-			if item_doc.project:
-				variants.set("project",item_doc.project)
+
+		d={}
+		for m in item_doc.attributes:
+			if m.attribute!="Item Group":
+				d[m.attribute]=m.attribute_value
 
 
-			if item_doc.image:
-				variants.set("image",item_doc.image)
+		d['Item Group']="Ready To Wear"
+		variants=create_variant_custom(item_doc.variant_of,d)
+		if item_doc.project:
+			variants.set("project",item_doc.project)
+
+		if item_doc.image:
+			variants.set("image",item_doc.image)
+
+		variants.set("item_group","Ready Stock")
+		check_item=frappe.db.sql("""select name from `tabItem` where item_code='{}'  """.format(variants.item_code),as_dict=1)
+		if len(check_item)!=0:
+			return check_item[0]['name']
+
+		variants.insert(ignore_permissions=True)
+		make_ptt_so(smpl=item_doc.name,new=variants.name)
+		make_bom(smpl=item_doc.name,new=variants.name)
+		return variants.name
 
 
-			variants.set("item_group","Ready Stock")
-			check_item=frappe.db.sql("""select name from `tabItem` where item_code='{}'  """.format(variants.item_code),as_dict=1)
-			if len(check_item)==0:
-				variants.insert(ignore_permissions=True)
-				if variants:
-					return variants.name
-			if len(check_item)!=0:
-				return check_item[0]['name']
 
-			if len(get_ptt)!=0:
-				for p in get_ptt:
-					docpt=frappe.get_doc("Pattern",p['name'])
-					dp=frappe.copy_doc(docpt)
-					dp.set("item_code",variants.name)
-					dp.set("workflow_state","Draft")
-					try:
-						dp.insert(ignore_permissions=True)
-						dp.submit()
-					except:
-						pass
 
-			if len(get_bom)!=0:
-				for k in get_bom:
-					bm=frappe.get_doc("BOM",k['name'])
-					d=frappe.copy_doc(bm)
-					print(variants.name,'aawwwwwww')
-					d.set("item",variants.name)
-					d.set('pattern_not_required',1)
-					d.set("workflow_state","Draft")
-					try:
-						d.insert(ignore_permissions=True)
-						d.submit()
-					except:
-						pass
-			
+
+
+@frappe.whitelist(allow_guest=True)
+def make_bom(smpl=None,new=None):
+	get_bom=frappe.db.sql("""select DISTINCT name from `tabBOM` where docstatus=1 and item='{}' and is_active=1 and is_default=1   """.format(smpl),as_dict=1)
+	if len(get_bom)!=0:
+		for k in get_bom:
+			print(k['name'],"mybom")
+			bm=frappe.get_doc("BOM",k['name'])
+			d=frappe.copy_doc(bm)
+			d.set("item",new)
+			d.set('pattern_not_required',1)
+			d.set("workflow_state","Draft")
+			try:
+				d.insert(ignore_permissions=True)
+				#d.submit()
+			except:
+				pass
+
+@frappe.whitelist(allow_guest=True)
+def make_ptt_so(smpl=None,new=None):
+	get_ptt=frappe.db.sql("""select name from `tabPattern` where docstatus=1 and item_code='{}'  """.format(smpl),as_dict=1)
+	if len(get_ptt)!=0:
+		for p in get_ptt:
+			docpt=frappe.get_doc("Pattern",p['name'])
+			dp=frappe.copy_doc(docpt)
+			dp.set("item_code",new)
+			dp.set("workflow_state","Draft")
+			try:
+				dp.insert(ignore_permissions=True)
+				dp.submit()
+			except:
+				continue
+
+
+
+
 
 
 @frappe.whitelist(allow_guest=True)
@@ -151,7 +177,7 @@ def update_stock(values=None):
 				for jk in get_stock:
 					if jk['actual_qty']>0:
 						net_stock_list.append(jk['actual_qty'])
-					
+
 			doc.set('net_stock_value',0)
 			doc.set("net_stock_value",sum(net_stock_list))
 			doc.save()
@@ -222,3 +248,68 @@ def make_rtw_item_project(items=None):
 
 			frappe.db.commit()
 			frappe.msgprint("Item created successfully")
+
+
+
+@frappe.whitelist(allow_guest=True)
+def make_customer_items(items=None,customer=None):
+	if not customer:
+		frappe.msgprint("Please select customer")
+		return
+
+	customer_count=[]
+	if len(customer)>1:
+		cs=customer.split(" ")
+		dx=[]
+		for c in cs:
+			dx.append(c[0])
+		if dx:
+			customer_count.append(dx[0]+dx[1])
+	else:
+		customer_count.append(c[0])
+
+
+
+	items=json.loads(items)
+	sizes=[]
+	all_item=[]
+	if frappe.db.exists("Item","Size"):
+		att=frappe.get_doc("Item Attribute","Size")
+		for j in att.item_attribute_values:
+			sizes.append(j.abbr)
+
+	for i in items:
+		doc=frappe.get_doc("Item",i)
+		if doc.item_group=="Sample" and doc.variant_of:
+			check_name=doc.name+"-"+customer_count[0]
+			if frappe.db.exists("Item",check_name):
+				return check_name
+			split_items=i.split("-")
+			sizes=list(set(sizes))
+			for s in split_items:
+				if s in sizes:
+					index =split_items.index(s)
+					split_items[index]="CI"
+				if "SMPL"==s:
+					split_items.remove(s)
+
+			name="-".join(split_items)
+
+			item_code=name+"-"+customer_count[0]
+			if frappe.db.exists("Item",item_code):
+				return item_code
+			d={'doctype':"Item","item_group":"Customise","project":doc.project ,"stock_uom":doc.stock_uom,"image":doc.image}
+			d['item_code']=item_code
+			d['item_name']=name+"-"+customer
+			d['parent_item']=doc.name
+			d['customise']=1
+			n=frappe.get_doc(d)
+			row =n.append("customer_list", {})
+			row.customer=customer
+			n.save(ignore_permissions=True)
+			make_pattren_from_variant_so(doc.name,n.name)
+			bom_copy_so_enabled_item(doc.name,n.name)
+			all_item.append(n.name)
+
+	if all_item:
+		return all_item[-1]
