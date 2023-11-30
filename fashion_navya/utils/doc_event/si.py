b5f2ec754e88705b14fed8878d14d97db6adad4c
Uttam <amiadlakha@gmail.com>
2023-11-30 13:10:54 +0530

    nov30

--- fashion_navya/utils/doc_event/si.py
+++ fashion_navya/utils/doc_event/si.py
@@ -1,1 +1,106 @@
 import frappe
+
+@frappe.whitelist(allow_guest=True)
+def make_new_si_id(doc,method):
+    split_date=doc.posting_date.split("-")
+    cyr=split_date[0]
+    next_year=str(int(cyr[2:])+1)
+    fyear=cyr+"-"+next_year
+    prefix="SINV"
+    used_no=[]
+    get_max=frappe.db.sql("""select max(nos) as no from `tabInvoice No series`  """,as_dict=1)
+    if get_max:
+        used_no.append(get_max[0]['no'])
+
+    if not doc.custom_invoice_no:
+        no_f=[]
+        cint=int(used_no[-1])+1
+        if len(str(cint))==3:
+            fno="00{}".format(cint)
+            no_f.append(fno)
+        if len(str(cint))==4:
+            fno="0{}".format(cint)
+            no_f.append(fno)
+
+        if len(str(cint))>4:
+            no_f.append(str(fno))
+
+        if not no_f:
+            no_f.append("000001")
+
+        final=prefix+"-"+fyear+"-"+no_f[0]
+        used_no.append(no_f[0])
+        get_si=frappe.db.sql("""select name from `tabSales Invoice` where custom_invoice_no='{}' """.format(final),as_dict=1)
+        if len(get_si)!=0:
+            frappe.throw("Invoice Series Error")
+
+        print(doc.name,"name")
+        print(doc.posting_date)
+        doc.set("custom_invoice_no",final)
+        get_s=frappe.db.sql("""select name from `tabInvoice No series` where series_no='{}'  """.format(final),as_dict=1)
+        if not get_s:
+            make_record_in(no=fno,status=doc.docstatus,name=doc.name,commit=0)
+            #frappe.db.commit()
+
+
+
+
+
+
+@frappe.whitelist(allow_guest=True)
+def make_new_si_id_old(year=0,months=0):
+    prefix="SINV"
+    fyear="2023-24"
+    used_no=["00527"]
+    get_all_in=frappe.db.sql("""select * from `tabSales Invoice`  where  custom_invoice_no is null and docstatus>0 and year(posting_date)='{}' and month(posting_date) ='{}'   ORDER BY posting_date """.format(year,months),as_dict=1)
+    if get_all_in:
+        for i in get_all_in:
+            si_doc=frappe.get_doc("Sales Invoice",i['name'])
+            #print(si_doc.name)
+            if not si_doc.custom_invoice_no:
+                get_no=[]
+                cint=int(used_no[-1])+1
+                if len(str(cint))==3:
+                    fno="00{}".format(cint)
+                if len(str(cint))==4:
+                    fno="0{}".format(cint)
+                final=prefix+"-"+fyear+"-"+fno
+                used_no.append(fno)
+                get_si=frappe.db.sql("""select name from `tabSales Invoice` where custom_invoice_no='{}' """.format(final),as_dict=1)
+                if len(get_si)!=0:
+                    frappe.throw("error")
+
+                print(si_doc.name,"name")
+                print(si_doc.posting_date)
+                frappe.db.sql(""" update `tabSales Invoice` set custom_invoice_no='{}' where name='{}'   """.format(final,si_doc.name))
+                #frappe.db.commit()
+
+                get_s=frappe.db.sql("""select name from `tabInvoice No series` where series_no='{}'  """.format(final),as_dict=1)
+                if not get_s:
+                    make_record_in(no=fno,status=si_doc.docstatus,name=si_doc.name,commit=1)
+                    frappe.db.commit()
+
+                print(final)
+
+
+@frappe.whitelist(allow_guest=True)
+def make_record_in(no=None,status=0,name=None,commit=0):
+    final="SINV-2023-24-{}".format(no)
+    d={"doctype":"Invoice No series","series_no":final}
+    d['prefix']="SINV"
+    d['fiscal_year']="2023-24"
+    d['nos']=str(no)
+    d['si_no']=name
+    d['booked']=1
+    if status==0:
+        d['series_status']="Draft"
+    if status==1:
+        d['series_status']="Submitted"
+    if status==2:
+        d['series_status']="cancelled"
+
+
+    doc=frappe.get_doc(d)
+    doc.insert(ignore_permissions=True)
+    if commit==1:
+        frappe.db.commit()
