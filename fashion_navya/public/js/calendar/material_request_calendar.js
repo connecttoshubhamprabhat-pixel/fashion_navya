b191f069cdd44ea8b86bac757a4d623995497830
Navya <amiadlakha@gmail.com>
2023-12-22 09:24:51 +0530

    dec22

--- fashion_navya/public/js/calendar/material_request_calendar.js
+++ fashion_navya/public/js/calendar/material_request_calendar.js
@@ -1,1 +1,22 @@
-consle.log('a')
+frappe.views.calendar["Material Request"] = {
+	field_map: {
+		"due_date": "schedule_date",
+		"id": "name",
+		"color": "color",
+		"title":"subject",
+		"status": "status"
+	},
+	gantt: {
+		field_map: {
+			"id": "name",
+			"due_date": "due_date",
+			"color": "color",
+			"title":"subject",
+			"status": "status"
+		}
+	},
+
+
+	get_events_method: "fashion_navya.utils.doc_event.mr.get_mr_details"
+};
+
