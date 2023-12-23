d093ecfea8146e53a761fcb8fe13aa2b5d7ac36b
Navya <amiadlakha@gmail.com>
2023-12-23 10:28:57 +0530

    dec23

--- fashion_navya/public/js/custom_script/work_order.js
+++ fashion_navya/public/js/custom_script/work_order.js
@@ -6,10 +6,10 @@ frappe.ui.form.on('Work Order',  {
     refresh: function(frm) {
         if (!cur_frm.doc.sales_order ){
           let user=frappe.session.user
-          let user_list=['pawasthy11@gmail.com','neha@navyacustom.com','sosowon@navyacustom.com','ksvwon@navyacustom.com']
-          if (user_list.includes(user)){
+         // let user_list=['pawasthy11@gmail.com','neha@navyacustom.com','sosowon@navyacustom.com','ksvwon@navyacustom.com']
+          //if (user_list.includes(user)){
             //$('.section-body').hide();
-          }
+          //}
 
         }
     }
