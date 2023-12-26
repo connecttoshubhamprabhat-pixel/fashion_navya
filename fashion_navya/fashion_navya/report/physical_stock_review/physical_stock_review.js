d44155074432f33ab717f018c8b5a7a6ed9ac84e
Uttam <amiadlakha@gmail.com>
2023-12-26 17:53:20 +0530

    dec26

--- fashion_navya/fashion_navya/report/physical_stock_review/physical_stock_review.js
+++ fashion_navya/fashion_navya/report/physical_stock_review/physical_stock_review.js
@@ -4,6 +4,21 @@
 
 frappe.query_reports["Physical Stock Review"] = {
 	"filters": [
+		{
+			fieldname: "phy",
+			label:"Physical Stock  Review",
+			fieldtype: "Link",
+			options: "Physical Stock  Review",
+			reqd: 1
+		},
+		{
+			fieldname: "diff_type",
+			label: __("Diff Qty Type"),
+			fieldtype: "Select",
+			default:"Plus",
+			options:["PLus","Minus","Both"],
+			reqd:1
+		},
 
 	]
 };
