16e31e47c1ec33624caa7bca88e55185b00f22af
Uttam <amiadlakha@gmail.com>
2024-01-06 14:21:31 +0530

    jan6

--- fashion_navya/utils/doc_event/api_1.py
+++ fashion_navya/utils/doc_event/api_1.py
@@ -403,3 +403,70 @@ def update_is_whatsapp():
                 frappe.db.commit()
             except:
                 continue
+
+
+
+@frappe.whitelist()
+def make_po_orders_project(items=None,values=None,project=None):
+    items=json.loads(items)
+    values=json.loads(values)
+    sitem=values.get("sitem")
+    supplier=values.get("supplier")
+    po_type=values.get("po_type")
+    sqty=values.get("sqty")
+    fg_qty=values.get("fg_qty")
+    rqdate=str(values.get("rqdate"))
+    if items:
+        d={"doctype":"Purchase Order","supplier":supplier,"is_subcontracted":1}
+        d['schedule_date']=rqdate
+        attributes_list=["DP","BP","HE","FBP"]
+        #supplier_dict={"Samsudeen Aakil Khan":"BPK","PRINTTECH":"DPK",""}
+        doc=frappe.get_doc(d)
+        for i in items:
+            main_item=i
+            attributes_kit=["k"]
+            split_parent=i.split("-")
+            for j in attributes_list:
+                if j in split_parent:
+                    attributes_kit.append(j)
+            for kit in attributes_kit:
+                row=doc.append("items", {})
+                row.item_code=sitem
+                print(sitem,'sitem')
+                row.qty=sqty
+                row.fg_item_qty=fg_qty
+                row.fg_parent=i
+                print(i,'i')
+                if kit=="k":
+                    if frappe.db.exists("Item",main_item+"-"+"k"):
+                        k1=main_item+"-"+"k"
+                        row.fg_item=k1
+                        print(k1,'k1')
+                    else:
+                        frappe.msgprint("Main Kit Item is missing for {}".format(i))
+                if kit=="DP":
+                    if frappe.db.exists("Item",main_item+"-"+"DPK"):
+                        k2=main_item+"-"+"DPK"
+                        row.fg_item=k2
+                        print(k2,"k2")
+                    else:
+                        frappe.msgprint("DPK Kit Item is missing for {}".format(i))
+                if kit=="BP":
+                    if frappe.db.exists("Item",main_item+"-"+"BPK"):
+                        k3=main_item+"-"+"BPK"
+                        row.fg_item=k3
+                        print(k3,"k3")
+
+                    else:
+                        frappe.msgprint("BPK Kit Item is missing for {}".format(i))
+
+                if kit=="HE":
+                    if frappe.db.exists("Item",main_item+"-"+"HEK"):
+                        k4=main_item+"-"+"HEK"
+                        row.fg_item=main_item+"-"+"HEK"
+                        print(k4,"k4")
+                    else:
+                        frappe.msgprint("Main Kit Item is missing for {}".format(i))
+        doc.insert()
+        print(doc.as_dict(),"dict")
+        frappe.msgprint("Created")
