bc2c2d22a61743bcf26d180e9eae565249052c2f
Navya <amiadlakha@gmail.com>
2024-03-11 14:10:43 +0530

    march11

--- fashion_navya/fashion_navya/report/timesheet_item_details/tsd.py
+++ fashion_navya/fashion_navya/report/timesheet_item_details/tsd.py
@@ -19,7 +19,7 @@ def execute(filters=None):
 	filters = frappe._dict(filters or {})
 	columns = get_columns()
 	data = get_data(filters)
-
+	
 	frappe.logger().debug(f"reportsss...... {data}")
 	return columns, data
 
@@ -31,43 +31,43 @@ def execute(filters=None):
 def get_conditions(filters):
 	conditions = ""
 	today =datetime.now().strftime('%Y-%m-%d')
-	first_day_of_this_month = date.today().replace(day=1)
+	first_day_of_this_month = date.today().replace(day=1) 
 	last_day_prev_month =str(first_day_of_this_month - timedelta(days=1))
-
+		
 	start_date=str(get_first_day(today))
 	start_date_last=str(get_first_day(last_day_prev_month))
 	plast =str(add_to_date(datetime.now(), days=-1, as_string=True))
-
+	
 	if filters.get("employee"):
 		conditions += "  and employee = %(employee)s"
-
+	
 	if filters.get("item"):
 		conditions += "  and item = %(item)s"
-
-
+	
+	
 	if filters.get("range"):
 		if filters.get("range")=="Last Day":
 			conditions += " and start_date>='{}' ".format(plast)
 			conditions += " and start_date<='{}' ".format(plast)
 
-
+			
 		if filters.get("range")=="Today":
 			from_time=today
 			conditions += " and start_date>='{}' ".format(today)
 			conditions += " and start_date<='{}' ".format(today)
-
+		
 		if filters.get("range")=="Current Month":
 			from_time=today
 			conditions += " and start_date>='{}' ".format(start_date)
 			conditions += " and start_date<='{}' ".format(today)
-
+		
 		if filters.get("range")=="Last Months":
 			from_time=today
 			conditions += " and start_date>='{}' ".format(start_date_last)
 			conditions += " and start_date<='{}' ".format(last_day_prev_month)
-
-
-
+		
+		
+		
 
 
 
@@ -80,78 +80,60 @@ def get_conditions(filters):
 
 def get_data(filters):
 	data = []
+	
 	conditions = get_conditions(filters)
-	records_emp=frappe.db.sql(
-		"""select DISTINCT employee,employee_name
+	records=frappe.db.sql(
+		"""select item, name,employee,start_date,employee_name,
+		total_hours
 		from `tabTimesheet`
-		where docstatus=1 %s   """
+		where docstatus=1 %s order by start_date  """
 		% conditions,
 		filters,
 		as_dict=1,
 	)
 
-	for i in records_emp:
-		conditions = get_conditions(filters)
-		if not filters.get("employee"):
-			conditions +="  and employee ='{}'  ".format(i.employee)
-
-		print(conditions,"con12qa")
-		records_item=frappe.db.sql(
-			"""select sum(total_hours) as total_hours,item
-			from `tabTimesheet`
-			where item is not null and docstatus=1 %s  GROUP BY item  """
-			% conditions,
-			filters,
-			as_dict=1,
-		)
-
-
-		for j in records_item:
-			print(j,"j")
-			d={}
-			#d['sd']=j.start_date
-			thours=round(j.total_hours,2)
-			if not frappe.db.exists("Item",j.item):
-				pass
-			if  not filters.get("item_type"):
-				print("hiqqqqqqqqqqqqqqqq")
-				d['item']=j.item
-				d['hours']=thours
+	for i in records:
+		d={}
+		if not frappe.db.exists("Item",i.item):
+			continue
+		if  not filters.get("item_type"):
+			d['item']=i.item
+			d['hours']=i.total_hours
+			d['employee']=i.employee
+			d['employee_name']=i.employee_name
+			d['ts']=i.name
+			data.append(d)
+			
+		if  filters.get("item_type") and i.item:
+			itemdoc=frappe.get_doc("Item",i.item)
+			if itemdoc.has_variants==1 and filters.get("item_type")=="template":
+				d['item']=i.item
+				d['hours']=i.total_hours
+				d['employee']=i.employee
+				d['employee_name']=i.employee_name
+				data.append(d)
+			
+			if itemdoc.has_variants==0 and itemdoc.variant_of and filters.get("item_type")=="variant":
+				d['item']=i.item
+				d['hours']=i.total_hours
+				d['employee']=i.employee
+				d['employee_name']=i.employee_name
+				data.append(d)
+			
+			if itemdoc.has_variants==0 and not itemdoc.variant_of and filters.get("item_type")=="Enabled" :
+				d['item']=i.item
+				d['hours']=i.total_hours
 				d['employee']=i.employee
 				d['employee_name']=i.employee_name
 				data.append(d)
 
-			if  filters.get("item_type") and j.item:
-				print("qqqqqqqqqqqqqqqqqqqqqqqqq2222222222222222")
-				itemdoc=frappe.get_doc("Item",j.item)
-				if itemdoc.has_variants==1 and filters.get("item_type")=="template":
-					d['item']=j.item
-					d['hours']=thours
-					d['employee']=i.employee
-					d['employee_name']=i.employee_name
-					data.append(d)
-
-				if itemdoc.has_variants==0 and itemdoc.variant_of and filters.get("item_type")=="variant":
-					d['item']=j.item
-					d['hours']=thours
-					d['employee']=i.employee
-					d['employee_name']=i.employee_name
-					data.append(d)
-
-				if itemdoc.has_variants==0 and not itemdoc.variant_of and filters.get("item_type")=="Enabled" :
-					d['item']=j.item
-					d['hours']=thours
-					d['employee']=i.employee
-					d['employee_name']=i.employee_name
-					data.append(d)
 
 
 
 
 
 
-
-
+	
 	return data
 def get_columns():
 	return [
@@ -190,13 +172,10 @@ def get_columns():
 			#"hidden": 1,
 			"width": 200,
 		},
-		{
-			"label": _("start date"),
-			"fieldtype": "data",
-			"fieldname": "sd",
-			"hidden": 1,
-			"width": 200,
-		},
-
-
+		
+			
 	]
+
+
+
+
